3c2886db11efdb5536f3d95e415a6e6d49a71132
0918nobita <nobita.0918@gmail.com>
2017-07-30 16:41:45 +0900

    Fix typo

--- src/net/zero918nobita/Xemime/X_Minus.java
+++ src/net/zero918nobita/Xemime/X_Minus.java
@@ -15,7 +15,7 @@ class X_Minus extends X_Object {
     @Override
     X_Object run(Environment env) throws Exception {
         X_Object o = obj.run(env);
-        if (o.getClass() != X_Int.class || o.getClass() != X_Double.class)
+        if (o.getClass() != X_Int.class && o.getClass() != X_Double.class)
             throw new Exception("数値以外のものには単項演算子を適用できません");
         if (o.getClass() == X_Int.class) return new X_Int(-((X_Int)o).getValue());
         return new X_Double(-((X_Double)o).getValue());
