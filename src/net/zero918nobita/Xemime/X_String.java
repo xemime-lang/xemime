544b57f7242fd2611abaeb97848f3628e0325da6
0918nobita <nobita.0918@gmail.com>
2017-08-06 20:26:00 +0900

    Implement add() to X_String

--- src/net/zero918nobita/Xemime/X_String.java
+++ src/net/zero918nobita/Xemime/X_String.java
@@ -21,4 +21,13 @@ class X_String extends X_Object {
     public boolean equals(Object obj) {
         return (obj instanceof X_String) && (this.value.equals(obj.toString()));
     }
+
+    @Override
+    X_String add(X_Object obj) throws Exception {
+        if (obj instanceof X_String) {
+            return new X_String(this.value + ((X_String) obj).value);
+        } else {
+            throw new Exception("String型オブジェクトに他の型のオブジェクトを加算することはできません");
+        }
+    }
 }
