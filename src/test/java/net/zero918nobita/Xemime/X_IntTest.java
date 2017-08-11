3322e4c2053e8abd1daab2a4403fb101d4d522cd
0918nobita <nobita.0918@gmail.com>
2017-08-11 22:29:11 +0900

    Add X_IntTest

---
+++ src/test/java/net/zero918nobita/Xemime/X_IntTest.java
@@ -0,0 +1,29 @@
+package net.zero918nobita.Xemime;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.is;
+
+/**
+ * net.zero918nobita.Xemime.X_Int のテストクラスです。
+ * @author Kodai Matsumoto
+ */
+
+public class X_IntTest {
+    @Test
+    public void testAbs() throws Exception {
+        X_Int i = (X_Int) (new X_Minus(new X_Int(256))).run();
+        X_Int abs = (X_Int) i.message(new X_Symbol("abs"), new ArrayList<>());
+        assertThat(i.getValue(), is(-256));
+        assertThat(abs.getValue(), is(256));
+    }
+
+    @Test
+    public void testToS() throws Exception {
+        X_String str = (X_String) new X_Int(2).message(new X_Symbol("to_s"), new ArrayList<>());
+        assertThat(str.toString(), is("2"));
+    }
+}
