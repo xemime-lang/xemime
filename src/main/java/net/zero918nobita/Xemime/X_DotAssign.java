e94c545a53decc63694c36aabc7064640479f588
0918nobita <nobita.0918@gmail.com>
2017-08-13 12:51:47 +0900

    Abolish declare expression of member of object

--- src/main/java/net/zero918nobita/Xemime/X_DotAssign.java
+++ src/main/java/net/zero918nobita/Xemime/X_DotAssign.java
@@ -20,10 +20,8 @@ class X_DotAssign extends X_Code {
     X_Code run() throws Exception {
         X_Code c1 = code1.run();
         X_Code c2 = code2.run();
-        if (c1 instanceof X_Handler && ((X_Handler) c1).hasMember(symbol)) {
+        if (c1 instanceof X_Handler) {
             ((X_Handler) c1).setMember(symbol, c2);
-        } else if (c1 instanceof X_Handler) {
-            throw new Exception("指定されたメンバは未宣言です");
         } else {
             throw new Exception("メンバを設定することができません");
         }
