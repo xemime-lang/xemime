aff1ce1306404f6c4436d6b7fdfb04ac33d23d9f
0918nobita <nobita.0918@gmail.com>
2017-08-20 09:43:40 +0900

    Add X_BlockTest

---
+++ src/test/java/net/zero918nobita/Xemime/X_BlockTest.java
@@ -0,0 +1,36 @@
+package net.zero918nobita.Xemime;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.util.ArrayList;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * net.zero918nobita.Xemime.X_Block のテストクラスです。
+ */
+
+public class X_BlockTest {
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    @Test
+    public void testRun() throws Exception {
+        ArrayList<X_Code> list = new ArrayList<X_Code>(){{
+            add(new X_BinExpr(0, TokenType.ADD, new X_Int(0, 1), new X_Int(0, 2)));
+        }};
+        X_Block block = new X_Block(0, list);
+        assertThat(block.run(), is(new X_Int(0, 3)));
+    }
+
+    @Test
+    public void testRun2() throws Exception {
+        expectedException.expect(Exception.class);
+        expectedException.expectMessage("0: ブロックの戻り値が記述されていません");
+        X_Block block = new X_Block(0, new ArrayList<>());
+        block.run();
+    }
+}
