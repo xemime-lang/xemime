42deec4b031a7590f14c7cc52c57341c3013ee46
0918nobita <nobita.0918@gmail.com>
2017-09-02 23:23:50 +0900

    Fix folder names

--- src/main/java/net/zero918nobita/Xemime/AST/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * (C) 2017 Kodai Matsumoto<br><br>
- *
- * Xemime AST ( Abstract Syntax Tree )<br><br>
- *
- * 注意事項
- */
-
-package net.zero918nobita.Xemime.ast;
---
+++ src/main/java/net/zero918nobita/Xemime/parser/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * (C) 2017 Kodai Matsumoto<br><br>
+ *
+ * Xemime Parser<br><br>
+ *
+ * JavaCC を用いて、入力として与えられたソースコードに対して構文解析を行い、
+ * 抽象構文木を生成します。<br><br>
+ *
+ * 抽象構文木の要素にあたる「ノード」のクラスについては、
+ * net.zero918nobita.Xemime.ast パッケージのドキュメントを参照してください。
+ */
+
+package net.zero918nobita.Xemime.parser;
