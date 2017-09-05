4d9429af2990992e2de35568656e41a0182effb4
0918nobita <nobita.0918@gmail.com>
2017-09-06 00:43:55 +0900

    ブロック式を使用した時にローカル環境が生成されない不具合を修正した

--- src/main/java/net/zero918nobita/Xemime/ast/BlockNode.java
+++ src/main/java/net/zero918nobita/Xemime/ast/BlockNode.java
@@ -1,11 +1,12 @@
 package net.zero918nobita.Xemime.ast;
 
+import net.zero918nobita.Xemime.interpreter.Main;
+
 import java.util.ArrayList;
 
 /**
- * ブロック
- * 戻り値を持ちます。
- * そのためブロック内に何も記述していない場合例外を発生させます。
+ * ブロック式を表すノードです。<br>
+ * 戻り値を持つため、ブロック内に何も記述していない場合例外を発生させます。
  * @author Kodai Matsumoto
  */
 
@@ -20,8 +21,10 @@ public class BlockNode extends Node {
     @Override
     public Node run() throws Exception {
         Node obj = null;
+        Main.loadLocalFrame(new X_Handler(0));
         if (list != null) for (Node o : list) obj = o.run();
         if (obj == null) throw new Exception(getLocation() + ": ブロックの戻り値が記述されていません");
+        Main.unloadLocalFrame();
         return obj;
     }
 }
