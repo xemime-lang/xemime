d358a12b1d8ffde41eed10bf46629ad4f7801f18
0918nobita <nobita.0918@gmail.com>
2017-09-17 07:49:49 +0900

    DotAssignNodeクラスのテストクラスを追加した

---
+++ src/test/java/net/zero918nobita/Xemime/DotAssignNodeTest.java
@@ -0,0 +1,31 @@
+package net.zero918nobita.Xemime;
+
+import net.zero918nobita.Xemime.interpreter.Main;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * net.zero918nobita.Xemime.ast.DotAssignNode のテストクラスです。
+ * @author Kodai Matsumoto
+ */
+
+public class DotAssignNodeTest {
+    private String br = System.lineSeparator();
+
+    @Test
+    public void test() throws Exception {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        Main.exec(
+                "let obj = Object.clone();\n" +
+                        "obj.a = 2;\n" +
+                        "println $ obj.a;\n"
+        );
+        assertThat(out.toString(), is("2" + br));
+    }
+}
