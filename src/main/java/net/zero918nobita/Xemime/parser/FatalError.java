9b73e61dfee532efc01c84a04cf76e1f39eaee01
0918nobita <nobita.0918@gmail.com>
2017-09-16 22:43:25 +0900

    FatalErrorのメッセージを修正した

--- src/main/java/net/zero918nobita/Xemime/parser/FatalError.java
+++ src/main/java/net/zero918nobita/Xemime/parser/FatalError.java
@@ -2,6 +2,6 @@ package net.zero918nobita.Xemime.parser;
 
 class FatalError extends Exception {
     FatalError(int location, int errorCode) {
-        super(location + ": インタプリタ内部の深刻な不具合が発生しました [" + errorCode + "]");
+        super(location + ": インタプリタ内部の深刻な不具合が発生しました。 [" + errorCode + "]");
     }
 }
