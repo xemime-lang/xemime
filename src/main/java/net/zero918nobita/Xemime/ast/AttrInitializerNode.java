4d49e03998e382fdc7f4619c630526d168081686
0918nobita <nobita.0918@gmail.com>
2017-09-20 23:02:51 +0900

    属性初期化式を表す AttrInitializerNode クラスを追加した

---
+++ src/main/java/net/zero918nobita/Xemime/ast/AttrInitializerNode.java
@@ -0,0 +1,12 @@
+package net.zero918nobita.Xemime.ast;
+
+/**
+ * 属性初期化式を表すノードです。
+ * @author Kodai Matsumoto
+ */
+
+public class AttrInitializerNode extends Node {
+    public AttrInitializerNode(int location) {
+        super(location);
+    }
+}
