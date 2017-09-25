70b71a605e14c4704c77b3e5044a313521fc9c94
0918nobita <nobita.0918@gmail.com>
2017-09-25 17:38:24 +0900

    SyntaxErrorクラスのアクセス権を修正した

--- src/main/java/net/zero918nobita/Xemime/parser/SyntaxError.java
+++ src/main/java/net/zero918nobita/Xemime/parser/SyntaxError.java
@@ -1,7 +1,7 @@
 package net.zero918nobita.Xemime.parser;
 
-class SyntaxError extends Exception {
-    SyntaxError(int location, int errorCode, String message) {
+public class SyntaxError extends Exception {
+    public SyntaxError(int location, int errorCode, String message) {
         super(location + ": 文法エラーです ( " + message + " ) [" + errorCode + "]");
     }
 }
