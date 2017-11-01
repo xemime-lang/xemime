da0c710f0375105ee97352f5f025a896702a342c
0918nobita <nobita.0918@gmail.com>
2017-11-01 22:38:17 +0900

    Update javadoc

--- src/main/java/net/zero918nobita/Xemime/parser/SyntaxError.java
+++ src/main/java/net/zero918nobita/Xemime/parser/SyntaxError.java
@@ -1,5 +1,10 @@
 package net.zero918nobita.Xemime.parser;
 
+/**
+ * 文法エラー
+ * @author Kodai Matsumoto
+ */
+
 class SyntaxError extends Exception {
     SyntaxError(int location, int errorCode, String message) {
         super(location + ": 文法エラーです ( " + message + " ) [" + errorCode + "]");
