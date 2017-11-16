c8bdfbc4ecc03019171957e67713e9891a72010a
0918nobita <nobita.0918@gmail.com>
2017-11-16 18:17:50 +0900

    テストクラスを更新した

--- src/test/java/net/zero918nobita/Xemime/DoubleTest.java
+++ src/test/java/net/zero918nobita/Xemime/DoubleTest.java
@@ -1,5 +1,6 @@
 package net.zero918nobita.Xemime;
 
+import net.zero918nobita.Xemime.ast.FatalException;
 import net.zero918nobita.Xemime.entity.Double;
 import net.zero918nobita.Xemime.entity.Int;
 import org.junit.Test;
@@ -25,5 +26,25 @@ public class DoubleTest {
         assertTrue(new Double(0.2).equals(new Double(0.20)));
         assertTrue(new Double(2.0).equals(new Int(2)));
         assertFalse(new Double(0.2).equals(new Double(2)));
+    }
+
+    @Test
+    public void testAdd() throws FatalException {
+        assertThat(new Double(0.2).add(new Double(0.3)).toString(), is("0.5"));
+    }
+
+    @Test
+    public void testSub() throws FatalException {
+        assertThat(new Double(0.2).sub(new Double(0.1)).toString(), is("0.1"));
+    }
+
+    @Test
+    public void testMultiply() throws FatalException {
+        assertThat(new Double(0.2).multiply(new Double(0.3)).toString(), is("0.06"));
+    }
+
+    @Test
+    public void testDivide() throws FatalException {
+        assertThat(new Double(0.2).divide(new Double(0.1)).toString(), is("2.0"));
     }
 }
