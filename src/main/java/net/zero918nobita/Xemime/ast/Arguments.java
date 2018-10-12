ad570b7324b48f751a9ad5293acd4d757605f21d
0918nobita <nobita.0918@gmail.com>
2018-10-12 11:41:18 +0900

    :art: Improve readability

--- src/main/java/net/zero918nobita/Xemime/ast/Arguments.java
+++ src/main/java/net/zero918nobita/Xemime/ast/Arguments.java
@@ -3,11 +3,7 @@ package net.zero918nobita.Xemime.ast;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 
-/**
- * 名前付き引数リスト または 名前無し引数リスト を表すノードです。
- * @author Kodai Matsumoto
- */
-
+/** 名前付き/無し引数リスト */
 public class Arguments {
     private LinkedHashMap<Symbol, Node> params;
 
@@ -19,22 +15,12 @@ public class Arguments {
         return params;
     }
 
-    /**
-     * 引数に渡された値を返します。名前付き引数リストの場合には定義したときの順番で指定してください。
-     * @param index 指定する引数の、定義時の引数リストでの順番 ( 0 から始まる )
-     * @return 値
-     */
     public Node getParam(int index) {
         ArrayList<Node> args = new ArrayList<>();
         params.forEach((symbol, node) -> args.add(node));
         return args.get(index);
     }
 
-    /**
-     * 引数に渡された値を返します。
-     * @param name 引数の名前
-     * @return 値
-     */
     public Node getParam(Symbol name) {
         return params.get(name);
     }
