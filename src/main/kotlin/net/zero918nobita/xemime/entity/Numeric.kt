3ccfde68283d3752e518b9f315f1f0ddb5e6f7a8
0918nobita <nobita.0918@gmail.com>
2018-10-12 18:16:49 +0900

    :pencil2: Fix typos

--- src/main/kotlin/net/zero918nobita/xemime/entity/Numeric.kt
+++ src/main/kotlin/net/zero918nobita/xemime/entity/Numeric.kt
@@ -3,6 +3,4 @@ package net.zero918nobita.xemime.entity
 import net.zero918nobita.xemime.ast.Node
 import kotlin.Int
 
-abstract class Numeric(location: Int): Node(location) {
-  lateinit var value: Number
-}
+open class Numeric(location: Int): Node(location)
